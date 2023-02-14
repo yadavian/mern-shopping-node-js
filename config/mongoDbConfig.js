aa1f5ebd95ca29cde613224ca7c958d1c7045781
yadavian <ankit.rama.yadav@gmail.com>
2023-02-14 03:30:07 -0800

    apis update

--- config/mongoDbConfig.js
+++ config/mongoDbConfig.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const { DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-const MONGO_DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+// const MONGO_DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const MONGO_DB_URL = `mongodb+srv://ankitramayadav:R3aHYqT4DJC1J7ye@cluster0.c9gqowa.mongodb.net/?retryWrites=true&w=majority/mern-shopping-app`;
 
 const connectDatabase = async () => {
   mongoose.set("strictQuery", true);
